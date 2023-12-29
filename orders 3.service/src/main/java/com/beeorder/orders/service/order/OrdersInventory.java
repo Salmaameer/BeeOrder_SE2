210bfd3e33f4ada1bb84feff2d4d348103ec478e
salma ameer <salmaameer409@gmail.com>
2023-12-30 00:20:07 +0200

    wla haga

---
+++ orders 3.service/src/main/java/com/beeorder/orders/service/order/OrdersInventory.java
@@ -0,0 +1,12 @@
+package com.beeorder.orders.service.order;
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class OrdersInventory {
+    List<Order> orders = new ArrayList<Order>();
+
+}
